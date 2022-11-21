414be3d5735728a8f534be8496d838397baba2ca
-Andreas <andreaschristensen2010@live.dk>
2022-11-21 14:14:47 +0100

    removed unused code

--- GTARoleplay/GTARoleplay/Library/MapDataLibrary.cs
+++ GTARoleplay/GTARoleplay/Library/MapDataLibrary.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.MemoryMappedFiles;
 
@@ -13,21 +12,6 @@ namespace GTARoleplay.Library
 
         /* Data file names */
         private const string HEIGHTDATA_FILE = @"data_files/GTAV_HeightMap_Data.data";
-        private const string SAFECOORDDATA_FILE = @"data_files/GTAV_SafeCoord_Data.data";
-
-        /* Object data file names and their max index */
-        public static string BURNEDCARSDATA_FILE = @"data_files/BurnedCarsCoords_Data.data";
-        public static string OUTSIDEDATA_FILE = @"data_files/OutsideObjectCoords_Data.data";
-        public static string GARBAGEDATA_FILE = @"data_files/GarbageObjectCoords_Data.data";
-        public static string ELECTRICALDATA_FILE = @"data_files/ElectricalObjectCoords_Data.data";
-
-        private static Dictionary<string, int> MaxObjectIndexes = new Dictionary<string, int>()
-        {
-            {BURNEDCARSDATA_FILE, 49 },
-            {OUTSIDEDATA_FILE, 7766 },
-            {GARBAGEDATA_FILE, 8262 },
-            {ELECTRICALDATA_FILE, 13477 }
-        };
 
         public static float GetHeightAtXY(float posX, float posY)
         {
@@ -58,52 +42,9 @@ namespace GTARoleplay.Library
             }
         }
 
-        public static Tuple<float, float> GetSafeCoordsAtXY(float posX, float posY)
-        {
-            if (!ContainPosition(posX, posY))
-                return null;
-
-            var mmf = MemoryMappedFile.CreateFromFile(SAFECOORDDATA_FILE, FileMode.Open);
-            using (mmf)
-            {
-                var x = (int)posX - (int)mapX.Item1;
-                var y = (long)(mapX.Item2 - mapX.Item1) * ((long)posY - (long)mapY.Item1);
-
-                using (var accessor = mmf.CreateViewAccessor((y + x) * (4 * 2), (4 * 2)))
-                {
-                    return new Tuple<float, float>(accessor.ReadSingle(0), accessor.ReadSingle(4));
-                }
-            }
-        }
-
         private static bool ContainPosition(float x, float y)
         {
             return mapX.Item1 <= x && x < 4100 && mapY.Item1 <= y && y <= mapY.Item2;
-        }
-
-        public static Tuple<float, float> GetStoredObjectCoords(int index, string fileName)
-        {
-            if (!ContainsObjectIndex(index, fileName))
-                return null;
-
-            var mmf = MemoryMappedFile.CreateFromFile(fileName, FileMode.Open);
-            using (mmf)
-            {
-                using (var accessor = mmf.CreateViewAccessor((index) * (4 * 2), (4 * 2)))
-                {
-                    return new Tuple<float, float>(accessor.ReadSingle(0), accessor.ReadSingle(4));
-                }
-            }
-        }
-
-        private static bool ContainsObjectIndex(int index, string fileName)
-        {
-            return index >= 0 && index < GetMaxObjectIndex(fileName);
-        }
-
-        public static int GetMaxObjectIndex(string fileName)
-        {
-            return MaxObjectIndexes.ContainsKey(fileName) ? MaxObjectIndexes[fileName] : 0;
         }
     }
 }
